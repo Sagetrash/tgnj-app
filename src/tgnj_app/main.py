e75c21348a35bbef47af12146a85f78e14637ab6
Sagetrash <sagetrash1435@proton.me>
2026-09-04 21:31:41 +0530

    fix: organized imports

--- src/tgnj_app/main.py
+++ src/tgnj_app/main.py
@@ -1,7 +1,10 @@
-from tgnj_app.gui.app import app
+import os
 import webbrowser
 from threading import Timer
-import os
+
+from tgnj_app.gui.app import app
+
+
 def open_browser():
     webbrowser.open_new_tab("http://127.0.0.1:5000")
 def main():
